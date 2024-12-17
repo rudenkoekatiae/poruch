ee6097ceb58d272780696bbc11ca2f345becb8f5
Sofia Bala <114072833+SophiaBala@users.noreply.github.com>
2024-12-17 20:39:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('searchInput').addEventListener('input', function () {
+    const searchValue = this.value.toLowerCase();
+    const info = document.querySelectorAll('.info');
+
+    info.forEach(info => {
+        const infoText = info.textContent.toLowerCase();
+        if (infoText.includes(searchValue)) {
+            info.style.display = 'block';
+        } else {
+            info.style.display = 'none'; 
+        }
+    });
+});
